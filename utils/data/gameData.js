e426eb14e7bdd644b69b4e089ba8c95be09ce42b
Daun Kim <yennakim11@gmail.com>
2024-05-28 19:54:18 -0500

    created createGame() and getGameTypes requests

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -7,5 +7,25 @@ const getGames = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createGame = (game) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/games`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(game),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getGameTypes = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/gametypes`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { getGames };
+export { getGames, createGame, getGameTypes };
