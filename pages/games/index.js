65680e28874de2e1589a2b50bfb5eb7c575edbaf
Daun Kim <yennakim11@gmail.com>
2024-06-01 01:48:02 -0500

    implemented game form component as well as new game form page

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 import GameCard from '../../components/game/GameCard';
 import { getGames } from '../../utils/data/gameData';
 
 function Home() {
   const [games, setGames] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     getGames().then((data) => setGames(data));
@@ -11,6 +14,12 @@ function Home() {
 
   return (
     <article className="games">
+      <Button onClick={() => {
+        router.push('/games/new');
+      }}
+      >
+        Register New Game
+      </Button>
       <h1>Games</h1>
       {games.map((game) => (
         <section key={`game--${game.id}`} className="game">
